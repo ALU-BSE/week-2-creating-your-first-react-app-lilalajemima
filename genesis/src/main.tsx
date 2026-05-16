df105d1623b5c21f0415e3643b377a9197c9b173
lilalajemima <169045583+lilalajemima@users.noreply.github.com>
2026-05-16 14:39:25 +0200

    Done with the activity added comments for project structure

--- genesis/src/main.tsx
+++ genesis/src/main.tsx
@@ -8,3 +8,7 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+// the entry point of the react app is index.tsx, which renders the app component into the root element of the HTML document. 
+// the app component is imported from App.tsx, which then imports MyComponent from MyComponent.tsx
+// the CSS files are also imported to style the components
